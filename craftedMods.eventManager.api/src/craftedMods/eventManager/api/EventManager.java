01fb95bd2f54116586d196cbd74fa9b670db4a03
CraftedMods <craftedMods@t-online.de>
2018-04-17 15:13:29 +0200

    Removed some unnecessary comments

--- craftedMods.eventManager.api/src/craftedMods/eventManager/api/EventManager.java
+++ craftedMods.eventManager.api/src/craftedMods/eventManager/api/EventManager.java
@@ -6,12 +6,8 @@ import org.osgi.annotation.versioning.ProviderType;
 
 @ProviderType
 public interface EventManager {
-	
-	/*
-	 * TODO: Only synchronous dispatched events support event results. The returned collection is never null.
-	 */
 
-	public Collection<EventProperties> dispatchEvent(EventInfo eventInfo, WriteableEventProperties properties);// The event properties will be locked
+	public Collection<EventProperties> dispatchEvent(EventInfo eventInfo, WriteableEventProperties properties);// The event properties will be locked. The returned collection is never null.
 
 	public Collection<EventProperties> dispatchEvent(EventInfo eventInfo);
 
