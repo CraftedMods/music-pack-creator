b4f678b90ad24b59705e1fb9b2e63cf4fd256391
CraftedMods <craftedMods@t-online.de>
2018-04-20 10:04:57 +0200

    Made the "contains" function more robust

--- craftedMods.utils.data/src/craftedMods/utils/data/ArrayUtils.java
+++ craftedMods.utils.data/src/craftedMods/utils/data/ArrayUtils.java
@@ -1,11 +1,14 @@
 package craftedMods.utils.data;
 
+import java.util.Objects;
+
 public class ArrayUtils {
 
 	public static <T> boolean contains(T[] array, T value) {
+		Objects.requireNonNull(array);
 		boolean contains = false;
 		for (T val : array) {
-			if (value.equals(val)) {
+			if (value == null ? val == null : value.equals(val)) {
 				contains = true;
 				break;
 			}
