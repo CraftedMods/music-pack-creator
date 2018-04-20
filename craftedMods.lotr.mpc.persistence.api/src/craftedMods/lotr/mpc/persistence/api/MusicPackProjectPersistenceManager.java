63524ed43172dd0b6c23ba00056d24c23a664289
CraftedMods <craftedMods@t-online.de>
2018-04-20 09:43:19 +0200

    "deleteMusicPackProject" not returns whether the project was deleted

--- craftedMods.lotr.mpc.persistence.api/src/craftedMods/lotr/mpc/persistence/api/MusicPackProjectPersistenceManager.java
+++ craftedMods.lotr.mpc.persistence.api/src/craftedMods/lotr/mpc/persistence/api/MusicPackProjectPersistenceManager.java
@@ -47,6 +47,6 @@ public interface MusicPackProjectPersistenceManager {
 	 * @param project
 	 *            The registered Music Pack Project
 	 */
-	public void deleteMusicPackProject(MusicPackProject project);
+	public boolean deleteMusicPackProject(MusicPackProject project);
 
 }
