c28df3b43da5ca2156348f4cfc363b778a4a0328
CraftedMods <craftedMods@t-online.de>
2018-05-27 19:49:51 +0200

    The Music Pack Project readers/writers now accept an Input/OutputStream

--- craftedMods.lotr.mpc.persistence.api/src/craftedMods/lotr/mpc/persistence/api/MusicPackProjectReader.java
+++ craftedMods.lotr.mpc.persistence.api/src/craftedMods/lotr/mpc/persistence/api/MusicPackProjectReader.java
@@ -1,24 +1,22 @@
 package craftedMods.lotr.mpc.persistence.api;
 
-import java.io.IOException;
-import java.nio.file.Path;
+import java.io.*;
 
 import org.osgi.annotation.versioning.ProviderType;
-
 import craftedMods.lotr.mpc.core.api.MusicPackProject;
 
 @ProviderType
 public interface MusicPackProjectReader {
 
 	/**
-	 * Reads a Music Pack Project from any location
+	 * Reads a Music Pack Project from any location. The supplied stream will be closed.
 	 * 
-	 * @param projectPath
-	 *            The location
+	 * @param projectData
+	 *            The read Music Pack Project data
 	 * @return The Music Pack Project
 	 * @throws IOException
 	 *             If the Music Pack Project couldn't be loaded due IO issues
 	 */
-	public MusicPackProject readMusicPackProject(Path projectPath) throws IOException;
+	public MusicPackProject readMusicPackProject(InputStream projectData) throws IOException;
 
 }
--- craftedMods.lotr.mpc.persistence.api/src/craftedMods/lotr/mpc/persistence/api/MusicPackProjectWriter.java
+++ craftedMods.lotr.mpc.persistence.api/src/craftedMods/lotr/mpc/persistence/api/MusicPackProjectWriter.java
@@ -1,25 +1,23 @@
 package craftedMods.lotr.mpc.persistence.api;
 
-import java.io.IOException;
-import java.nio.file.Path;
+import java.io.*;
 
 import org.osgi.annotation.versioning.ProviderType;
-
 import craftedMods.lotr.mpc.core.api.MusicPackProject;
 
 @ProviderType
 public interface MusicPackProjectWriter {
 
 	/**
-	 * Writes the Music Pack Project to any location
+	 * Writes the Music Pack Project to any location. The supplied stream will be closed.
 	 * 
 	 * @param project
 	 *            The Music Pack Project
-	 * @param location
-	 *            The location
+	 * @param output
+	 *            The stream to which the Music Pack Project data will be written
 	 * @throws IOException
 	 *             If the Music Pack Project couldn't be saved due IO issues
 	 */
-	public void writeMusicPackProject(MusicPackProject project, Path location) throws IOException;
+	public void writeMusicPackProject(MusicPackProject project, OutputStream output) throws IOException;
 
 }
